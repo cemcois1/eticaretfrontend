947ad7670410a15e3856c2ad62edaaf5e571e61b
cemal can yıldırım <56302638+cemcois1@users.noreply.github.com>
2022-05-09 15:55:42 +0300

    hotfix

--- src/Components/Product.js
+++ src/Components/Product.js
@@ -5,13 +5,13 @@ const Product = (props) => {
     console.log(props.imageRef)
     return <div className="product">
 
-        <div className="colorise">
+        <div className="Image">
 
         </div>
         <div className="product__description">
             <div className="product__description_header"> {props.title}</div>
             <br/>
-            <div> {props.price}$</div>
+            <div> {props.price}₺</div>
 
         </div>
 
