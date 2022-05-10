1a5bf37362050a0566ed611db1d4b88689b62c5a
cemal can yıldırım <56302638+cemcois1@users.noreply.github.com>
2022-05-10 15:26:56 +0300

    added order css

--- src/Components/Cart/Order/Order.js
+++ src/Components/Cart/Order/Order.js
@@ -1,5 +1,7 @@
+import "./Order.css"
+
 const Order = (props) => {
-    return <div>
+    return <div className="Order">
         <h6>order key is : {props.productkey} </h6>
         <h6>order count is {props.counter}</h6>
     </div>
